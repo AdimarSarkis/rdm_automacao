d0d46b2cb0c247a76ecd2cc4a421cbc65c8654d4
Adimar Sarkis <sarkis170@gmail.com>
2023-02-25 15:04:57 -0300

    Feito o componente footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,29 @@
+import "./Footer.css"
+import {FiFacebook, FiInstagram, FiYoutube} from "react-icons/fi"
+
+const Footer = () => {
+  return(
+    <div className='footer'>
+      <div className='locais'>
+        <span><strong>Matriz</strong></span>
+        <span> Rua Veneza, 279 - Jardim Europa </span>
+        <span>Rio Branco - AC</span>  
+        <span>Fone (68) 3227-1305 / 3226-2038</span>
+        <span>Email: suporte.ac@automacaordm.com.br</span> 
+        <span>ciss-riobranco@ciss.com.br</span>
+      </div>
+      <div className='locais'>
+        <span><strong>Filial</strong></span>
+        <span>Manaus - AM</span>  
+        <span>Fone (92) 9-9192-2826 / 99365-0996</span>
+        <span>Email: suporte.am@automacaordm.com.br</span> 
+      </div>
+      <div className='social'>
+        <a href="#" target="_blank" rel="noopener noreferrer"><FiFacebook/></a>
+        <a href="#" target="_blank" rel="noopener noreferrer"><FiInstagram/></a>
+        <a href="#" target="_blank" rel="noopener noreferrer"><FiYoutube/></a>
+      </div>
+    </div>
+  );
+}
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import Cards from '../components/Cards/index.jsx';
 import ConteudoHome from "../components/ConteudoHome/index.jsx";
 import Header from "../components/Header/index.jsx";
-
+import Footer from "../components/Footer/index";
 const Home = () => {
   return (
     <>
       <Header />
       <ConteudoHome />
+      <Footer />
     </>
   );
 };
