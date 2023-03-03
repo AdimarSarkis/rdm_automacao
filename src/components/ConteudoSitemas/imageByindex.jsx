356dbcfdbb0ff495b535c74272025d2515831405
Adimar Sarkis <sarkis170@gmail.com>
2023-03-02 22:59:09 -0300

    bug fixed

--- src/components/ConteudoSitemas/imageByindex.jsx
+++ src/components/ConteudoSitemas/imageByindex.jsx
@@ -1,6 +1,6 @@
 import atacarejo from '../../assets/atacarejo.png'
 import selfcheckout from '../../assets/selfcheckout-slide.png'
-import cissBox from '../../assets/cissBox.jpg'
+import cissBox from '../../assets/cissbox.jpg'
 import radar from '../../assets/radar.jpg'
 
 export const images = [atacarejo, selfcheckout, cissBox, radar]
@@ -8,4 +8,4 @@ export const text = ["Atacarejo", "Selfcheckout", "CissBox" ,"Radar XMl"]
 const imageByIndex = (index) => images[index % images.length]
 export const textByIndex = (index) => text[index % text.length]
 
-export default imageByIndex
+export default imageByIndex;
